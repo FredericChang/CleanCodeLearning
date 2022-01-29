26c07abdd0d6abfd506dd08ab12e01ff1c43bdec
Frederic <paulpoiu123@gmail.com>
2022-01-29 21:29:11 +0100

    Added New Test (Page69)

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -38,19 +38,32 @@ namespace TestProject1
             game = new Game();
         }
         [Test]
-        public void TestScoreNoThrows()
+        public void TestOneThrow()
         {
-            Game f = new Game();
-            f.Add(5);
-            Assert.AreEqual(5, f.Score);
-            f.Add(4);
-            Assert.AreEqual(9, f.Score);
-            f.Add(7);
-            f.Add(2);
-            Assert.AreEqual(18, f.Score);
-            Assert.AreEqual(9, f.ScoreForFrame(1));
-            Assert.AreEqual(18, f.ScoreForFrame(2));
-
+            game.Add(5);
+            Assert.AreEqual(5, game.Score);
         }
+        [Test]
+        public void TestSecondThrow()
+        {
+            game.Add(5);
+            game.Add(4);
+            Assert.AreEqual(9, game.Score);
+        }
+        //[Test]
+        //public void TestScoreNoThrows()
+        //{
+        //    Game f = new Game();
+        //    f.Add(5);
+        //    Assert.AreEqual(5, f.Score);
+        //    f.Add(4);
+        //    Assert.AreEqual(9, f.Score);
+        //    f.Add(7);
+        //    f.Add(2);
+        //    Assert.AreEqual(18, f.Score);
+        //    Assert.AreEqual(9, f.ScoreForFrame(1));
+        //    Assert.AreEqual(18, f.ScoreForFrame(2));
+
+        //}
     }
 }
